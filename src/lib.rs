feb3a61bafc178a1f0fda6008d107edfb4c731d2
Branden Kunkel <kunkel.branden6130@gmail.com>
2024-02-24 19:39:32 -0800

    merging remote and local

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,78 @@
 // Lib for interacting with polygon.io
 
+#![allow(unused_imports)] //Remove for release build
+#![allow(dead_code)]
+
 pub mod parameter_handles{
 
-    use std::fs::File;
-    use std::io::Read;
-    use std::path::Path;
+    use std::any::Any;
+    use std::fmt::{Debug, Display};
+    use std::fs::{read_to_string, File};
+    use std::io::{BufRead, BufReader, Read};
+    use std::ops::Index;
+    use std::path::{self, Path};
+    use std::vec;
+    use toml::{Table, Value};
+    use toml::map::Map;
 
-    pub fn read_file(file_path: &str){
 
-        let yaml_file_path = Path::new(file_path);
+    pub fn create_toml_table(file_path: &str) -> Table{
+ 
 
-        let mut yaml_file: File = match File::open(&yaml_file_path) {
-            
-            Err(why) => panic!("Could not open file: {}\n{}", &yaml_file_path.display(), why),
+        let toml_file_path = Path::new(&file_path);
+        let mut toml_str_buffer = String::new();
+
+        let mut toml_file: File = match File::open(&toml_file_path) {
+
+            Err(why) => panic!("Could not open file: {}!\nReason: {}", &toml_file_path.display(), why),
             Ok(file) => file,
+            
         };
 
-        let mut yaml_string = String::new();
+        let mut _read_toml_to_string = match toml_file.read_to_string(&mut toml_str_buffer) {
 
-        let _file_to_str_buffer = yaml_file.read_to_string(&mut yaml_string);
+            Err(why) => panic!("File: {} was opened but could not be read!\nReason: {}", &toml_file_path.display(), why),
+            Ok(string) => string,
+            
+        }; 
 
-        println!("{}", yaml_string)
+        let toml_table: Table = match toml_str_buffer.parse() {
 
+            Err(why) => panic!("Could not read File: {} to Table!\nReason: {}", &toml_file_path.display(), why),
+            Ok(table) => table,
+            
+        };
+
+        toml_table
+        
+
+        }
+
+
+    pub fn get_file_paths(paths_table: Table) { 
+
+        for header in paths_table.keys(){
+
+                let header_str: &str = "program_files";
+
+                if header == header_str {
+
+                    println!("{}", header);
+
+                }
+                
+            }
+
+        }
     }
 
+        
 
-}
+        
+ 
+
+    
+
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use poly_api;
+#![allow(unused_imports)]
+
+use poly_api::parameter_handles;
 
 fn main(){
 
-    poly_api::parameter_handles::read_file("/home/brandenk/Documents/programming/rust/rustpoly/src/program_files/settings.toml");
+println!("Working!");
+
+let table = parameter_handles::create_toml_table("/home/brandenk/Documents/programming/rust/rustpoly/src/program_files/file_paths.toml");
+parameter_handles::get_file_paths(table);
 
 }
